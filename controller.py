f4366b6f30970b2cb6eb574cea71260a4b05db04
Jocelyn-Hn <jocelyn07198@gmail.com>
2023-04-30 23:11:49 -0500

    Modified controller code. Need to display '*' when string value entered first rather than incorrect integer.

--- controller.py
+++ controller.py
@@ -91,153 +91,116 @@ class Controller(QMainWindow, Ui_MainWindow):
             self.error_d4.setText('')
 
     def roll(self):
-        if self.checkBox_d100.isChecked():
-            try:
+        try:
+            if self.checkBox_d100.isChecked():
                 rolls = []
                 if int(self.input_d100.text()) > 10 or int(self.input_d100.text()) < 1:
-                    self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
                     self.error_d100.setText('*')
-                    self.rolls_d100.setText('')
-                    self.total_d100.setText('')
+                    raise ValueError
                 else:
+                    self.error_d100.setText('')
+                    self.instructions.setText('')
                     for roll in range(int(self.input_d100.text())):
                         rolls.append(randint(1, 101))
                         self.rolls_d100.setText(', '.join([str(roll) for roll in rolls]))
                         self.total_d100.setText(str(sum(rolls)))
-                    self.error_d100.setText('')
 
-            except ValueError:
-                self.instructions.setText('Please enter a whole number. (*)')
-                self.error_d100.setText('*')
-                self.rolls_d100.setText('')
-                self.total_d100.setText('')
-
-        if self.checkBox_d20.isChecked():
-            try:
+            if self.checkBox_d20.isChecked():
                 rolls = []
                 if int(self.input_d20.text()) > 10 or int(self.input_d20.text()) < 1:
-                    self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
                     self.error_d20.setText('*')
-                    self.rolls_d20.setText('')
-                    self.total_d20.setText('')
+                    raise ValueError
                 else:
+                    self.error_d20.setText('')
+                    self.instructions.setText('')
                     for roll in range(int(self.input_d20.text())):
                         rolls.append(randint(1, 21))
                         self.rolls_d20.setText(', '.join([str(roll) for roll in rolls]))
                         self.total_d20.setText(str(sum(rolls)))
-                    self.error_d20.setText('')
 
-            except ValueError:
-                self.instructions.setText('Please enter a whole number. (*)')
-                self.error_d20.setText('*')
-                self.rolls_d20.setText('')
-                self.total_d20.setText('')
-
-        if self.checkBox_d12.isChecked():
-            try:
+            if self.checkBox_d12.isChecked():
                 rolls = []
                 if int(self.input_d12.text()) > 10 or int(self.input_d12.text()) < 1:
-                    self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
                     self.error_d12.setText('*')
-                    self.rolls_d12.setText('')
-                    self.total_d12.setText('')
+                    raise ValueError
                 else:
+                    self.error_d12.setText('')
+                    self.instructions.setText('')
                     for roll in range(int(self.input_d12.text())):
                         rolls.append(randint(1, 13))
                         self.rolls_d12.setText(', '.join([str(roll) for roll in rolls]))
                         self.total_d12.setText(str(sum(rolls)))
-                    self.error_d12.setText('')
 
-            except ValueError:
-                self.instructions.setText('Please enter a whole number. (*)')
-                self.error_d12.setText('*')
-                self.rolls_d12.setText('')
-                self.total_d12.setText('')
-
-        if self.checkBox_d10.isChecked():
-            try:
+            if self.checkBox_d10.isChecked():
                 rolls = []
                 if int(self.input_d10.text()) > 10 or int(self.input_d10.text()) < 1:
-                    self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
                     self.error_d10.setText('*')
-                    self.rolls_d10.setText('')
-                    self.total_d10.setText('')
+                    raise ValueError
                 else:
+                    self.error_d10.setText('')
+                    self.instructions.setText('')
                     for roll in range(int(self.input_d10.text())):
                         rolls.append(randint(1, 11))
                         self.rolls_d10.setText(', '.join([str(roll) for roll in rolls]))
                         self.total_d10.setText(str(sum(rolls)))
-                    self.error_d10.setText('')
 
-            except ValueError:
-                self.instructions.setText('Please enter a whole number. (*)')
-                self.error_d10.setText('*')
-                self.rolls_d10.setText('')
-                self.total_d10.setText('')
-
-        if self.checkBox_d8.isChecked():
-            try:
+            if self.checkBox_d8.isChecked():
                 rolls = []
                 if int(self.input_d8.text()) > 10 or int(self.input_d8.text()) < 1:
-                    self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
                     self.error_d8.setText('*')
-                    self.rolls_d8.setText('')
-                    self.total_d8.setText('')
+                    raise ValueError
                 else:
+                    self.error_d8.setText('')
+                    self.instructions.setText('')
                     for roll in range(int(self.input_d8.text())):
                         rolls.append(randint(1, 9))
                         self.rolls_d8.setText(', '.join([str(roll) for roll in rolls]))
                         self.total_d8.setText(str(sum(rolls)))
-                    self.error_d8.setText('')
 
-            except ValueError:
-                self.instructions.setText('Please enter a whole number. (*)')
-                self.error_d8.setText('*')
-                self.rolls_d8.setText('')
-                self.total_d8.setText('')
-
-        if self.checkBox_d6.isChecked():
-            try:
+            if self.checkBox_d6.isChecked():
                 rolls = []
                 if int(self.input_d6.text()) > 10 or int(self.input_d6.text()) < 1:
-                    self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
                     self.error_d6.setText('*')
-                    self.rolls_d6.setText('')
-                    self.total_d6.setText('')
+                    raise ValueError
                 else:
+                    self.error_d6.setText('')
+                    self.instructions.setText('')
                     for roll in range(int(self.input_d6.text())):
                         rolls.append(randint(1, 7))
                         self.rolls_d6.setText(', '.join([str(roll) for roll in rolls]))
                         self.total_d6.setText(str(sum(rolls)))
-                    self.error_d6.setText('')
 
-            except ValueError:
-                self.instructions.setText('Please enter a whole number. (*)')
-                self.error_d6.setText('*')
-                self.rolls_d6.setText('')
-                self.total_d6.setText('')
-
-        if self.checkBox_d4.isChecked():
-            try:
+            if self.checkBox_d4.isChecked():
                 rolls = []
                 if int(self.input_d4.text()) > 10 or int(self.input_d4.text()) < 1:
-                    self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
                     self.error_d4.setText('*')
-                    self.rolls_d4.setText('')
-                    self.total_d4.setText('')
+                    raise ValueError
                 else:
+                    self.error_d4.setText('')
+                    self.instructions.setText('')
                     for roll in range(int(self.input_d4.text())):
                         rolls.append(randint(1, 5))
-
                         self.rolls_d4.setText(', '.join([str(roll) for roll in rolls]))
                         self.total_d4.setText(str(sum(rolls)))
-                    self.error_d4.setText('')
+        except ValueError:
+            self.instructions.setText('Please input a dice amount from 1 - 10. (*)')
+            self.error_clear()
 
-            except ValueError:
-                self.instructions.setText('Please enter a whole number. (*)')
-                self.error_d4.setText('*')
-                self.rolls_d4.setText('')
-                self.total_d4.setText('')
+    def error_clear(self):
+        self.rolls_d100.setText('')
+        self.rolls_d20.setText('')
+        self.rolls_d12.setText('')
+        self.rolls_d10.setText('')
+        self.rolls_d8.setText('')
+        self.rolls_d6.setText('')
+        self.rolls_d4.setText('')
+        self.total_d100.setText('')
+        self.total_d20.setText('')
+        self.total_d12.setText('')
+        self.total_d10.setText('')
+        self.total_d8.setText('')
+        self.total_d6.setText('')
+        self.total_d4.setText('')
 
     def clear(self):
         self.checkBox_d100.setChecked(False)
